a707ae4cd0da95709096a53b797555ee3fb5f10d
Ihor Stanovyi <ihor.stanovyi@gmail.com>
2025-10-27 00:22:22 +0200

    feat: implement streams functions

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import { createWriteStream } from "fs";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-  // Write your code here
+  const filePath = join(__dirname, "files", "fileToWrite.txt");
+  const writeStream = createWriteStream(filePath);
+
+  process.stdin.pipe(writeStream);
+
+  writeStream.on("error", (err) => {
+    throw err;
+  });
 };
 
 await write();
