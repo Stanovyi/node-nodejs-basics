23442dd3851a261eb024332c7392fc9ce08653e9
Ihor Stanovyi <ihor.stanovyi@gmail.com>
2025-10-27 00:21:27 +0200

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-  // Write your code here
+  const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+  const hash = createHash("sha256");
+  const stream = createReadStream(filePath);
+
+  stream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
+
+  stream.on("error", (err) => {
+    throw err;
+  });
 };
 
 await calculateHash();
